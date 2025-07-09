ed2540faa34bbfcea6e13112b3042d8aaff6422f
Julius Ceaser <trilochanbhusal000@gmail.com>
2025-07-09 18:37:36 +0545

    fix: update application title to reflect new branding

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const APP_TITLE = "Kerabari Sites Management";
+export const APP_TITLE = "Ninja Sites Management";
 export const DATABASE_PREFIX = "acme";
 export const TEST_DB_PREFIX = "test_acme";
 
